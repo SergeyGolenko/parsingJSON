154046cf46589308c58d26bf89271956728c8231
SergeyGolenko <1989sergiotay@gmail.com>
2020-11-13 14:19:21 +0200

    done

--- ParseJSON(practice swiftBook)/ViewController.swift
+++ ParseJSON(practice swiftBook)/ViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
+
+
+struct ThreeCitiesWeather: Decodable {
+    
+    var minsk: City?
+    var madrid: City?
+    var riga: City?
+    
+}
+
+struct City: Decodable {
+    var temperature: String
+    var weather: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        let urlString = "http://swiftbook.ru/json/JSONdata_swift4.json"
+        
+        guard let url = URL(string: urlString) else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else {return} //проверяю data на nil
+            guard error == nil else {return} //если есть ошибка то прекращаю работу
+            
+            do{
+                let threeCitiesWeather = try JSONDecoder().decode(ThreeCitiesWeather.self, from: data)
+                print(threeCitiesWeather)
+            } catch let error {}
+             print(error)
+            
+            
+        }
+    .resume()
+        
+  
     }
 
 
